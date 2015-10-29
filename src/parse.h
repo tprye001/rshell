9b03578ed6c717cd50e56497d7fe579173ee0299
Jonathan Alcaraz <jalca011@hammer.cs.ucr.edu>
2015-10-29 16:14:03 -0700

    started 'parse.h'

---
+++ src/parse.h
@@ -0,0 +1,12 @@
+#include <string>
+#include <queue>
+#include "ABC.h"
+
+std::queue<ABC> parse (std::string line) {
+  std::queue<ABC> ret;
+  for (int c = 0; c < line.size(); c++) {
+    if (line.at(c) == '#')
+      c = line.size();
+
+  }
+}
