cdb6561ff9a0bcdefb4d4932273c7f422c655112
Tricia Pryer <tprye001@hammer.cs.ucr.edu>
2015-10-29 18:45:19 -0700

    trying to do some parsing

--- src/parse.h
+++ src/parse.h
@@ -1,12 +1,17 @@
 #include <string>
 #include <queue>
+#include <cstring>
 #include "ABC.h"
 
 std::queue<ABC> parse (std::string line) {
   std::queue<ABC> ret;
-  for (int c = 0; c < line.size(); c++) {
+ /* for (int c = 0; c < line.size(); c++) {
     if (line.at(c) == '#')
       c = line.size();
     else if (line.at(c) == ';' || line.at(c) == ''
+  }*/
+  while(1){
+   char* sbstg = strtok(line, " ");
+
   }
 }
