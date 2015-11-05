158cff9e07e087b611f18ce45fd1827d5b7f9404
Tricia Pryer <tprye001@hammer.cs.ucr.edu>
2015-11-05 01:43:57 -0800

    fork mkdir and echo

--- src/echo.cpp
+++ src/echo.cpp
@@ -1,12 +1,33 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <cstdio>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (int argc, char * argv[]) {
   const char * file = "echo";
-  
-  execvp(file, argv);
-  perror("execve failed");
+
+  pid_t c_pid, pid;
+  int status;
+
+  c_pid = fork();
+
+  if(c_pid < 0){
+    perror("fork failed");
+    exit(1);
+  }
+
+  else if(c_pid == 0){
+    execvp(file, argv);
+    perror("execve failed");
+  }
+
+  else if(c_pid > 0){
+    if((pid = wait(&status)) < 0){
+      perror("wait");
+      exit(1);
+    }
+  }
 
   return 0;
 }
--- src/mkdir.cpp
+++ src/mkdir.cpp
@@ -1,12 +1,30 @@
 #include <cstdlib>
 #include <cstdio>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main (int argc, char * argv[]) {
   const char * file = "mkdir";
 
-  execvp(file, argv);
-  perror("execve failed");
+  pid_t c_pid, pid;
+  int status;
 
+  c_pid = fork();
+
+  if( c_pid < 0){
+    perror("fork failed");
+    exit(1);
+  }
+  else if(c_pid == 0){
+    execvp(file, argv);
+    perror("execve failed");
+  }
+  else if(c_pid > 0){
+    if((pid = wait(&status)) < 0){
+      perror("wait");
+      exit(1);
+    }
+  }
   return 0;
 }
