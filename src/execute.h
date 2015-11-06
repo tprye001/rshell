5d53e169e06ebbaca0aa7e91dcfa730ad8a41b5b
Tricia Pryer <tprye001@hammer.cs.ucr.edu>
2015-11-06 00:42:38 -0800

    correct return value in execute

--- src/execute.h
+++ src/execute.h
@@ -12,15 +12,6 @@
 bool execute(std::string cmd, std::vector<std::string> args){
 
   //turn args into one big string
-  // unsigned n = args.size();
-  /*char* sArgs[args.size()]; //should be n not 20 but WERROR
-  for(unsigned int i = 0; i < args.size(); i++){
-    char sArgsData = *args.at(i).data();
-    char* sArgsPointer = &sArgsData;
-    sArgs[i] = sArgsPointer;
-    
-  }*/
-  
   std::vector<char *> sArgs;
 
   for (std::vector<std::string>::iterator loop = args.begin(); 
@@ -40,19 +31,21 @@ bool execute(std::string cmd, std::vector<std::string> args){
   if (c_pid < 0) {
     perror("fork failed");
     exit(1);
+    return false;
   }
 
   else if (c_pid ==0) {
     execvp(cmd.data(), &sArgs[0]);
     perror("execvp fail");
+    return false;
   }
 
   else if (c_pid > 0) {
     if( (pid = wait(&status)) < 0) {
       perror("wait");
       exit(1);
+      return true;
     }
   }
-    
-  return true;
+  return false;
 }
