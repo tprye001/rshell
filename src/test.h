b107e30bbf03a169ccc979614b8126d340ba97f1
Tricia Pryer <tprye001@hammer.cs.ucr.edu>
2015-11-15 16:03:22 -0800

    yay! test works nowgit add -A woohoo! good job team

--- src/test.h
+++ src/test.h
@@ -7,36 +7,45 @@
 #include <vector>
 
 bool test(std::vector<char *> args){
-  std::string flag1 = "-f";
-  std::string flag2 = "-e";
-  std::string flag3 = "-d";
+  char flag1 = 'f';
+  char flag2 = 'e';
+  char flag3 = 'd';
+  
+  bool ret = false;
 
-  char* flag = &flag2[0];
+  char flag;
   char* path;
-  if(*(args.at(0)) == flag1[0]) {
-    flag = &flag1[0];
-    path = args.at(1);
+
+  if(args.at(1)[1] == flag1) {
+    flag = flag1;
+    path = args.at(2);
   }
-  else if(*(args.at(0)) == flag3[0]){
-    flag = &flag3[0];
-    path = args.at(1);
+  else if(args.at(1)[1] == flag3){
+    flag = flag3;
+    path = args.at(2);
   }
-  else if(*(args.at(0)) == flag2[0]){
-    path = args.at(1);
+  else if(args.at(1)[1] == flag2){
+    flag = flag2;
+    path = args.at(2);
   }
-  else
-    path = args.at(0);
+  else{
+    path = args.at(1);
+    flag = flag2;
+  }
 
   struct stat kevin;
   stat(path, &kevin);
 
-  if(*flag == flag1[0]){
-    return S_ISREG(kevin.st_mode);
+  if(flag == flag1){
+    ret = S_ISREG(kevin.st_mode);
   }
-  else if(*flag == flag3[0]){
-    return S_ISDIR(kevin.st_mode);
+  else if(flag == flag3){
+    ret =  S_ISDIR(kevin.st_mode);
   }
-  return (S_ISREG(kevin.st_mode) || S_ISDIR(kevin.st_mode));
+  else {
+    ret = (S_ISREG(kevin.st_mode) || S_ISDIR(kevin.st_mode));
+  }
+  return ret;
 }
 
 #endif
